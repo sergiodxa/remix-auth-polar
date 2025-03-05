b544f52fb518339ff18fa13e0a58371d717bf00b
Sergio Xalambrí <hello@sergiodxa.com>
2025-03-04 20:01:08 -0500

    Refactor PolarStrategy to include default cookie value and define Scope type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { CodeChallengeMethod, OAuth2Strategy } from "remix-auth-oauth2";
 import type { Strategy } from "remix-auth/strategy";
 
+// biome-ignore lint/performance/noBarrelFile: This is ok here
+export {
+	UnexpectedErrorResponseBodyError,
+	OAuth2RequestError,
+	UnexpectedResponseError,
+} from "remix-auth-oauth2";
+
 export class PolarStrategy<User> extends OAuth2Strategy<User> {
 	override name = "polar";
 
@@ -14,7 +21,7 @@ export class PolarStrategy<User> extends OAuth2Strategy<User> {
 				clientId: options.clientId,
 				clientSecret: options.clientSecret,
 				codeChallengeMethod: CodeChallengeMethod.S256,
-				cookie: options.cookie,
+				cookie: options.cookie ?? "polar",
 				redirectURI: options.redirectURI,
 				scopes: options.scopes,
 				tokenEndpoint: "https://api.polar.sh/v1/oauth2/token",
@@ -26,56 +33,57 @@ export class PolarStrategy<User> extends OAuth2Strategy<User> {
 }
 
 export namespace PolarStrategy {
+	export type Scope =
+		| "openid"
+		| "profile"
+		| "email"
+		| "user:read"
+		| "organizations:read"
+		| "organizations:write"
+		| "custom_fields:read"
+		| "custom_fields:write"
+		| "discounts:read"
+		| "discounts:write"
+		| "checkout_links:read"
+		| "checkout_links:write"
+		| "checkouts:read"
+		| "checkouts:write"
+		| "products:read"
+		| "products:write"
+		| "benefits:read"
+		| "benefits:write"
+		| "events:read"
+		| "events:write"
+		| "meters:read"
+		| "meters:write"
+		| "files:read"
+		| "files:write"
+		| "subscriptions:read"
+		| "subscriptions:write"
+		| "customers:read"
+		| "customers:write"
+		| "customer_sessions:write"
+		| "orders:read"
+		| "refunds:read"
+		| "refunds:write"
+		| "metrics:read"
+		| "webhooks:read"
+		| "webhooks:write"
+		| "external_organizations:read"
+		| "license_keys:read"
+		| "license_keys:write"
+		| "repositories:read"
+		| "repositories:write"
+		| "issues:read"
+		| "issues:write"
+		| "customer_portal:read"
+		| "customer_portal:write";
+
 	export interface ConstructorOptions
 		extends Pick<
 			OAuth2Strategy.ConstructorOptions,
 			"clientId" | "clientSecret" | "redirectURI" | "cookie"
 		> {
-		scopes: Array<
-			| "openid"
-			| "profile"
-			| "email"
-			| "user:read"
-			| "organizations:read"
-			| "organizations:write"
-			| "custom_fields:read"
-			| "custom_fields:write"
-			| "discounts:read"
-			| "discounts:write"
-			| "checkout_links:read"
-			| "checkout_links:write"
-			| "checkouts:read"
-			| "checkouts:write"
-			| "products:read"
-			| "products:write"
-			| "benefits:read"
-			| "benefits:write"
-			| "events:read"
-			| "events:write"
-			| "meters:read"
-			| "meters:write"
-			| "files:read"
-			| "files:write"
-			| "subscriptions:read"
-			| "subscriptions:write"
-			| "customers:read"
-			| "customers:write"
-			| "customer_sessions:write"
-			| "orders:read"
-			| "refunds:read"
-			| "refunds:write"
-			| "metrics:read"
-			| "webhooks:read"
-			| "webhooks:write"
-			| "external_organizations:read"
-			| "license_keys:read"
-			| "license_keys:write"
-			| "repositories:read"
-			| "repositories:write"
-			| "issues:read"
-			| "issues:write"
-			| "customer_portal:read"
-			| "customer_portal:write"
-		>;
+		scopes: Array<Scope>;
 	}
 }
